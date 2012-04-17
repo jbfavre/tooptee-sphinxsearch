c168d985457fc99c65228f65c1399a6fe4322171
Jean Baptiste Favre <github@jbfavre.org>
2012-04-17 16:27:47 +0200

    Add $remote_fs dependency to init script. Avoids lintian warning.

--- debian/sphinxsearch-common.init.d
+++ debian/sphinxsearch-common.init.d
@@ -20,8 +20,8 @@
 ### BEGIN INIT INFO
 # Provides:          sphinx
 # Required-Start:    $network $local_fs $remote_fs
-# Required-Stop:     
-# Should-Start:      $named
+# Required-Stop:     $network $local_fs $remote_fs
+# Should-Start:      
 # Should-Stop:       
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
