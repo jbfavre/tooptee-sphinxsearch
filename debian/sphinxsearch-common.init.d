4ea06000ba7e8b065e8d8e067c6aa40d8736fdea
Jean Baptiste Favre <github@jbfavre.org>
2012-04-20 22:30:50 +0200

    Added modified init script, logrotate config file, change postinstall script & default file. Add sample config file in common package instead of doc one.

--- debian/sphinxsearch-common.init.d
+++ debian/sphinxsearch-common.init.d
@@ -1,274 +1,171 @@
-#!/bin/sh 
+#! /bin/sh
 #
-# Copyright (c) 2007 Javier Fernandez-Sanguino <jfs@debian.org>
+#		Written by Miquel van Smoorenburg <miquels@cistron.nl>.
+#		Modified for Debian
+#		by Ian Murdock <imurdock@gnu.ai.mit.edu>.
+#               Further changes by Javier Fernandez-Sanguino <jfs@debian.org>
+#               Modified for sphinx by Radu Spineanu <radu@debian.org>
 #
-# This is free software; you may redistribute it and/or modify
-# it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2,
-# or (at your option) any later version.
 #
-# This is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License with
-# the Debian operating system, in /usr/share/common-licenses/GPL;  if
-# not, write to the Free Software Foundation, Inc., 59 Temple Place,
-# Suite 330, Boston, MA 02111-1307 USA
-#
+
 ### BEGIN INIT INFO
-# Provides:          sphinx
-# Required-Start:    $network $local_fs
-# Required-Stop:     
-# Should-Start:      $named
-# Should-Stop:       
+# Provides:          sphinxsearch
+# Required-Start:    $local_fs $remote_fs $syslog $network $time
+# Required-Stop:     $local_fs $remote_fs $syslog $network
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: SphinxSearch
-# Description:       free open-source SQL full-text search engine
+# Short-Description: Fast standalone full-text SQL search engine
 ### END INIT INFO
 
+
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-DAEMON=/usr/bin/searchd       # Introduce the server's location here
-NAME=#PACKAGE                 # Introduce the short server's name here
-DESC=#PACKAGE                 # Introduce a short description here
-LOGDIR=/var/log/sphinxsearch  # Log directory to use
-
-PIDFILE=/var/run/$NAME.pid 
+DAEMON=/usr/bin/searchd
+NAME=sphinxsearch
+DESC=sphinxsearch
 
 test -x $DAEMON || exit 0
-test -x $DAEMON_WRAPPER || exit 0
 
-. /lib/lsb/init-functions
+LOGDIR=/var/log/sphinxsearch
+PIDFILE=/var/run/sphinxsearch/searchd.pid
+DODTIME=1                   # Time to wait for the server to die, in seconds
+                            # If this value is set too low you might not
+                            # let some servers to die gracefully and
+                            # 'restart' will not work
 
-# Default options, these can be overriden by the information
-# at /etc/default/$NAME
-DAEMON_OPTS=""          # Additional options given to the server 
+STARTDELAY=0.5
 
-DODTIME=10              # Time to wait for the server to die, in seconds
-                        # If this value is set too low you might not
-                        # let some servers to die gracefully and
-                        # 'restart' will not work
-                        
-LOGFILE=$LOGDIR/$NAME.log  # Server logfile
-#DAEMONUSER=sphinx   # Users to run the daemons as. If this value
-                        # is set start-stop-daemon will chuid the server
-
-# Include defaults if available
-if [ -f /etc/default/$NAME ] ; then
-	. /etc/default/$NAME
+# Include sphinxsearch defaults if available
+if [ -f /etc/default/sphinxsearch ] ; then
+    . /etc/default/sphinxsearch
 fi
 
-# Use this if you want the user to explicitly set 'RUN' in 
-# /etc/default/
-if [ "x$RUN" != "xyes" ] ; then
-    log_failure_msg "$NAME disabled, please adjust the configuration to your needs "
-    log_failure_msg "and then set RUN to 'yes' in /etc/default/$NAME to enable it."
-    exit 1
-fi
-
-# Check that the user exists (if we set a user)
-# Does the user exist?
-if [ -n "$DAEMONUSER" ] ; then
-    if getent passwd | grep -q "^$DAEMONUSER:"; then
-        # Obtain the uid and gid
-        DAEMONUID=`getent passwd |grep "^$DAEMONUSER:" | awk -F : '{print $3}'`
-        DAEMONGID=`getent passwd |grep "^$DAEMONUSER:" | awk -F : '{print $4}'`
-    else
-        log_failure_msg "The user $DAEMONUSER, required to run $NAME does not exist."
-        exit 1
-    fi
+if [ "$START" != "yes" ]; then
+  echo "To enable $NAME, edit /etc/default/sphinxsearch and set START=yes"
+  exit 0
 fi
 
 
 set -e
 
-running_pid() {
-# Check if a given process pid's cmdline matches a given name
+# Make sure the pidfile directory exists with correct permissions
+piddir=`dirname "$PIDFILE"`
+if [ ! -d "$piddir" ]; then
+    mkdir -p "$piddir"
+    chown -R sphinxsearch "$piddir"
+    chgrp -R sphinxsearch "$piddir"
+fi
+
+
+running_pid()
+{
+    # Check if a given process pid's cmdline matches a given name
     pid=$1
     name=$2
-    [ -z "$pid" ] && return 1 
+    [ -z "$pid" ] && return 1
     [ ! -d /proc/$pid ] &&  return 1
     cmd=`cat /proc/$pid/cmdline | tr "\000" "\n"|head -n 1 |cut -d : -f 1`
-    # Is this the expected server
+    # Is this the expected child?
     [ "$cmd" != "$name" ] &&  return 1
     return 0
 }
 
-running() {
+running()
+{
 # Check if the process is running looking at /proc
 # (works for all users)
 
     # No pidfile, probably no daemon present
     [ ! -f "$PIDFILE" ] && return 1
+    # Obtain the pid and check it against the binary name
     pid=`cat $PIDFILE`
-    running_pid $pid $DAEMON_WRAPPER || return 1
+    running_pid $pid $DAEMON || return 1
     return 0
 }
 
-start_server() {
-# Start the process using the wrapper
-        if [ -z "$DAEMONUSER" ] ; then
-            start-stop-daemon --start --quiet --pidfile $PIDFILE \
-                        --exec $DAEMON -- $DAEMON_OPTS
-            errcode=$?
-        else
-# if we are using a daemonuser then change the user id
-            start-stop-daemon --start --quiet --pidfile $PIDFILE \
-                        --chuid $DAEMONUSER \
-                        --exec $DAEMON -- $DAEMON_OPTS
-            errcode=$?
+do_force_stop() {
+# Forcefully kill the process
+    [ ! -f "$PIDFILE" ] && return
+    if running ; then
+        kill -15 $pid
+        # Is it really dead?
+        [ -n "$DODTIME" ] && sleep "$DODTIME"s
+        if running ; then
+            kill -9 $pid
+            [ -n "$DODTIME" ] && sleep "$DODTIME"s
+            if running ; then
+                echo "Cannot kill $NAME (pid=$pid)!"
+                exit 1
+            fi
         fi
-	return $errcode
+    fi
+    rm -f $PIDFILE
+    return 0
 }
-
-stop_server() {
-# Stop the process using the wrapper
-        if [ -z "$DAEMONUSER" ] ; then
-            start-stop-daemon --stop --quiet --pidfile $PIDFILE \
-                        --exec $DAEMON
-            errcode=$
-        else
-# if we are using a daemonuser then look for process that match
-            start-stop-daemon --stop --quiet --pidfile $PIDFILE \
-                        --user $DAEMONUSER \
-                        --exec $DAEMON
-            errcode=$
+do_start() {
+        # Check if we have the configuration file
+        if [ ! -f /etc/sphinxsearch/sphinx.conf ]; then
+            echo "\n"
+            echo "Please create an /etc/sphinxsearch/sphinx.conf configuration file."
+            echo "A template is provided as /etc/sphinxsearch/sphinx.conf.sample."
+            exit 1
         fi
 
-	return $errcode
+        start-stop-daemon --start --pidfile $PIDFILE --chuid sphinxsearch --exec ${DAEMON}
 }
-
-reload_server() {
-    [ ! -f "$PIDFILE" ] && return 1
-    pid=`cat $PIDFILE` # This is the daemon's pid
-    # Send a SIGHUP
-    kill -1 $pid
-    return $?
+do_stop() {
+        start-stop-daemon --stop --quiet --oknodo --user sphinxsearch --pidfile $PIDFILE \
+            --exec $DAEMON
 }
-
-force_stop() {
-# Force the process to die killing it manually
-	[ ! -e "$PIDFILE" ] && return
-	if running ; then
-		kill -15 $pid
-	# Is it really dead?
-		sleep "$DIETIME"s
-		if running ; then
-			kill -9 $pid
-			sleep "$DIETIME"s
-			if running ; then
-				echo "Cannot kill $NAME (pid=$pid)!"
-				exit 1
-			fi
-		fi
-	fi
-	rm -f $PIDFILE
-}
-
 
 case "$1" in
   start)
-	log_daemon_msg "Starting $DESC " "$NAME"
-        # Check if it's running first
-        if running ;  then
-            log_progress_msg "apparently already running"
-            log_end_msg 0
-            exit 0
-        fi
-        if start_server && running ;  then
-            # It's ok, the server started and is running
-            log_end_msg 0
-        else
-            # Either we could not start it or it is not running
-            # after we did
-            # NOTE: Some servers might die some time after they start,
-            # this code does not try to detect this and might give
-            # a false positive (use 'status' for that)
-            log_end_msg 1
-        fi
-	;;
-  stop)
-        log_daemon_msg "Stopping $DESC" "$NAME"
+        echo -n "Starting $DESC: "
+        do_start
+        [ -n "$STARTDELAY" ] && sleep $STARTDELAY
+
         if running ; then
-            # Only stop the server if we see it running
-            stop_server
-            log_end_msg $?
+            echo "$NAME."
         else
-            # If it's not running don't do anything
-            log_progress_msg "apparently not running"
-            log_end_msg 0
-            exit 0
+            echo " ERROR."
         fi
+        ;;
+  stop)
+        echo -n "Stopping $DESC: "
+        do_stop
+        echo "$NAME."
         ;;
   force-stop)
-        # First try to stop gracefully the program
-        $0 stop
-        if running; then
-            # If it's still running try to kill it more forcefully
-            log_daemon_msg "Stopping (force) $DESC" "$NAME"
-            force_stop
-            log_end_msg $?
-        fi
-	;;
-  restart|force-reload)
-        log_daemon_msg "Restarting $DESC" "$NAME"
-        stop_server
-        # Wait some sensible amount, some server need this
-        [ -n "$DIETIME" ] && sleep $DIETIME
-        start_server
-        running
-        log_end_msg $?
-	;;
-  status)
-
-        log_daemon_msg "Checking status of $DESC" "$NAME"
-        if running ;  then
-            log_progress_msg "running"
-            log_end_msg 0
+        echo -n "Forcefully stopping $DESC: "
+        do_force_stop
+        if ! running ; then
+            echo "$NAME."
         else
-            log_progress_msg "apparently not running"
-            log_end_msg 1
-            exit 1
+            echo " ERROR."
         fi
         ;;
-  # Use this if the daemon cannot reload
-  reload)
-        log_warning_msg "Reloading $NAME daemon: not implemented, as the daemon"
-        log_warning_msg "cannot re-read the config file (use restart)."
+  restart|reload|force-reload)
+    echo -n "Restarting $DESC: "
+        do_stop
+        [ -n "$DODTIME" ] && sleep $DODTIME
+        do_start
+        echo "$NAME."
         ;;
-  # And this if it cann
-  #reload)
-          #
-          # If the daemon can reload its config files on the fly
-          # for example by sending it SIGHUP, do it here.
-          #
-          # If the daemon responds to changes in its config file
-          # directly anyway, make this a do-nothing entry.
-          #
-          # log_daemon_msg "Reloading $DESC configuration files" "$NAME"
-          # if running ; then
-          #    reload_server
-          #    if ! running ;  then
-          # Process died after we tried to reload
-          #       log_progress_msg "died on reload"
-          #       log_end_msg 1
-          #       exit 1
-          #    fi
-          # else
-          #    log_progress_msg "server is not running"
-          #    log_end_msg 1
-          #    exit 1
-          # fi
-                                                                                    #;;
 
+  status)
+    echo -n "$NAME is "
+    if running ;  then
+        echo "running"
+    else
+        echo " not running."
+        exit 1
+    fi
+    ;;
   *)
-	N=/etc/init.d/$NAME
-	echo "Usage: $N {start|stop|force-stop|restart|force-reload|status}" >&2
-	exit 1
-	;;
+    N=/etc/init.d/$NAME
+    # echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
+    echo "Usage: $N {start|stop|restart|force-reload|status|force-stop}" >&2
+    exit 1
+    ;;
 esac
 
 exit 0
